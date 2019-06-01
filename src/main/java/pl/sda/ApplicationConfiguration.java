6f62be3acea659ec10a852870e247e38520a06fa
Bartek Kokoszko <bartol857@gmail.com>
2019-06-01 10:07:42 +0200

    add do.properties

--- src/main/java/pl/sda/ApplicationConfiguration.java
+++ src/main/java/pl/sda/ApplicationConfiguration.java
@@ -1,22 +1,37 @@
 package pl.sda;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan("pl.sda")
+@PropertySource("db.properties")
 public class ApplicationConfiguration {
+
+    @Value("${db.driverClassName}")
+    private String dbDriverClassName;
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUsername;
+
+    @Value("${db.password}")
+    private String dbPassword;
 
     @Bean
     public DriverManagerDataSource dataSource(){
     DriverManagerDataSource ret = new DriverManagerDataSource();
-    ret.setDriverClassName("org.h2.Driver");
-    ret.setUsername("sa");
-    ret.setPassword("");
-    ret.setUrl("jdbc:h2:tcp://localhost/~/test");
+    ret.setDriverClassName(dbDriverClassName);
+    ret.setUsername(dbUsername);
+    ret.setPassword(dbPassword);
+    ret.setUrl(dbUrl);
 
     return ret;
 
